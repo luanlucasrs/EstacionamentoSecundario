186938b07838c3b21478d8ef34d2c64cea487f4e
renanalmeids <58474539+renanalmeids@users.noreply.github.com>
2021-05-15 19:36:09 -0300

    [6] Logica inicial da interface grafica

--- src/projetoFinalOO/Estacionamento.java
+++ src/projetoFinalOO/Estacionamento.java
@@ -6,12 +6,15 @@ package projetoFinalOO;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Estacionamento {
 
 	private List<Cliente> clientes; 
 	private List<Veiculo> veiculosNaoMensalista;
 	private List<ControleGaragem> garagem;
 	private Cobranca cobranca; 
+	// teste
 	
 	public Estacionamento () {
 		clientes = new ArrayList<Cliente>();
@@ -28,6 +31,8 @@ public class Estacionamento {
 		
 		Estacionamento estacionamento = new Estacionamento();
 		 
+		rodarMenu();
+
 		
 		if (!estacionamento.cadastrarCliente(null)) {
 			throw new DadosPessoaisIncompletoException();
@@ -43,7 +48,81 @@ public class Estacionamento {
 		}
 		
 	}
+
+	/* Sequência de código para criação do menu de visualização da interface
+	 *
+	 * Funcionamento
+	 * 1) Int menu: primeira tela 
+	 * 2) void rodarMenu: funcionamento do menu  
+	 * 3) rodarMenu: chamada na main 
+	 * 
+	 */
+	public static int menu(){
+		//Variável para botão 
+		int valorDoBotao = 0;
+		
+		// Alocar botoes 
+		Object[]  funcoes = { "Consultar Placa","Cadastrar Veículo", "Cadastrar Cliente", "Vincular Veículo", "Registrar Entrada", 
+				"Registrar Saída", "Cobrar"};
+		int funcao = JOptionPane.showOptionDialog(null, "Escolha uma opção", "O que deseja fazer?", JOptionPane.DEFAULT_OPTION,
+				JOptionPane.WARNING_MESSAGE, null, funcoes, funcoes[0]);
+		
+		//Consultar Placa
+		if (funcao == 0 ) {
+			
+		}
+		
+		//Cadastrar Veiculo
+		if (funcao == 1) {
+			
+		}
+		
+		//Cadastrar Cliente 
+		if (funcao == 2) {
+			
+		}
+		
+		//Vincular Veículo 
+		if (funcao == 3) {
+			
+		}
+		
+		//Registrar Entrada
+		if (funcao == 4) {
+			
+		}
+		
+		//Registrar Saída 
+		if (funcao == 5) {
+			
+		}
+		
+		//Cobrar 
+		if (funcao == 6) {
+			
+		}
+		
+		// Cancelar operação 
+		else if (funcao == -1){
+			valorDoBotao = -1;
+		}
+		
+		return valorDoBotao;
+	}
 	
+	/**
+	 * Função responsável peleo funcionamento do menu até que a interface seja inserida 
+	 */
+	private static void rodarMenu() {
+		int condicao = menu();
+		while (condicao >=0 ) {
+			condicao = menu();
+		}
+		
+	}
+
+	
+
 	
 	public Veiculo consultarPlaca(String placa) {
 		return new Veiculo(); 
